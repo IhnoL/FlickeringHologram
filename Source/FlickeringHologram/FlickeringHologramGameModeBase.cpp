e12222cd35afacf206bb8b17cae92f1870ffffe8
Ihno <ihno.luebbers@maibornwolff.de>
2021-04-17 13:53:36 +0200

    streams texture on object

---
+++ Source/FlickeringHologram/FlickeringHologramGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "FlickeringHologramGameModeBase.h"
+
---
+++ Source/FlickeringHologram/FlickeringHologramGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlickeringHologramGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLICKERINGHOLOGRAM_API AFlickeringHologramGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
